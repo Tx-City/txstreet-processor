dc1d53a4ef4c42046347cd74714eb5c119b1dd9e
dev <ethomas@posteo.net>
2022-06-29 00:49:36 +0000

    delete blocks fix

--- src/data/chains.ts
+++ src/data/chains.ts
@@ -1,27 +1,40 @@
 export const chainConfig: any = {
     ETH: {
         wikiname: 'ethereum',
-        storeBlockFile: true
+        storeBlockFile: true,
+        deleteBlocksOlderThanSeconds: 90000,
+        txsCollection: true
     },
     BTC: {
         wikiname: 'bitcoin',
-        storeBlockFile: true
+        storeBlockFile: true,
+        deleteBlocksOlderThanSeconds: 90000,
+        txsCollection: true
     },
     LTC: {
         wikiname: 'litecoin',
-        storeBlockFile: true
+        storeBlockFile: true,
+        deleteBlocksOlderThanSeconds: 90000,
+        txsCollection: true
     },
     XMR: {
         wikiname: 'monero',
-        storeBlockFile: true
+        storeBlockFile: true,
+        deleteBlocksOlderThanSeconds: 90000,
+        txsCollection: true
     },
     BCH: {
         wikiname: 'bitcoincash',
-        storeBlockFile: true
+        storeBlockFile: true,
+        deleteBlocksOlderThanSeconds: 90000,
+        txsCollection: true
     },
     ARBI: {
         wikiname: 'arbitrum',
         rollup: true,
-        storeBlockFile: false
+        storeBlockFile: false,
+        deleteBlocksOlderThanSeconds: 3600,
+        deleteBlocksAmount: 200,
+        txsCollection: false
     }
 }
--- src/lib/tasks2/jobs/common/workers/deleteBlocksAndConfTxs.ts
+++ src/lib/tasks2/jobs/common/workers/deleteBlocksAndConfTxs.ts
@@ -9,7 +9,6 @@ import path from 'path';
 const dataDir = path.join(process.env.DATA_DIR as string || '/mnt/disks/txstreet_storage');
 const chain: string = workerData.chain;
 const keepMinBlocks = 300;
-const deleteAmount = 4;
 
 setInterval(async () => {
     try {
@@ -17,21 +16,26 @@ setInterval(async () => {
         const collection = database.collection('blocks');
 
         const count = await collection.find({ chain }).count();
-        if (isNaN(count) || count <= keepMinBlocks + deleteAmount) {
+        if (isNaN(count) || count <= keepMinBlocks + (chainConfig[chain].deleteBlocksAmount || 4)) {
             Logger.print(`${chain} has ${count} blocks in the db. Cancel deletion.`);
             return;
         }
-        const txCollection = database.collection("transactions_" + chain);
-        const oldBlocks = await collection.find({ chain }).sort({ height: 1 }).limit(deleteAmount).toArray();
+        const oldBlocks = await collection.find({ chain }).project({ _id: 1, timestamp: 1, height: 1, hash: 1, transactions: 1 }).sort({ height: 1 }).limit(chainConfig[chain].deleteBlocksAmount || 4).toArray();
         for (let i = 0; i < oldBlocks.length; i++) {
             const block = oldBlocks[i];
-            if(Math.round(Date.now()/1000) - Number(block.timestamp) < 90000){ //25 hours
-                Logger.info("Block " + block.height + " less than 25 hours ago. Skipping deletion.")
+            let timestamp = Number(block.timestamp);
+            if(timestamp > 9999999999) timestamp /= 1000;
+            let now = Math.round(Date.now() / 1000);
+            if (now - timestamp < (chainConfig[chain].deleteBlocksOlderThanSeconds || 90000)) { //25 hours
+                Logger.info("Block " + block.height + " less than " + (chainConfig[chain].deleteBlocksOlderThanSeconds || 90000) + " seconds ago. Skipping deletion.", now, timestamp, now - timestamp);
                 continue;
             }
             const transactions = block?.transactions || [];
-            const txDelete = await txCollection.deleteMany({ hash: { $in: transactions } });
-            Logger.print(`Deleted ${txDelete.deletedCount} of ${transactions.length} txs from block ` + block.hash);
+            if (chainConfig[chain].txsCollection) {
+                const txCollection = database.collection("transactions_" + chain);
+                const txDelete = await txCollection.deleteMany({ hash: { $in: transactions } });
+                Logger.print(`Deleted ${txDelete.deletedCount} of ${transactions.length} txs from block ` + block.hash);
+            }
             const blockDelete = await collection.deleteOne({ _id: block._id });
             if (blockDelete.deletedCount) {
                 Logger.print(`Deleted block ` + block.hash);
@@ -41,7 +45,7 @@ setInterval(async () => {
             }
 
             //delete block file from NFS
-            if(!chainConfig[chain].storeBlockFile) return;
+            if (!chainConfig[chain].storeBlockFile) continue;
             const firstPart = block.hash[block.hash.length - 1];
             const secondPart = block.hash[block.hash.length - 2];
             const filePath = path.join(dataDir, 'blocks', chain, firstPart, secondPart, block.hash);
