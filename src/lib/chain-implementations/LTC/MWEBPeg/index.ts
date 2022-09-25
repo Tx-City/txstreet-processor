22ccce35214768f7b41e36cdefd635aa6044bf57
dev <ethomas@posteo.net>
2022-09-25 19:36:08 +0000

    mweb values

--- src/lib/chain-implementations/LTC/MWEBPeg/index.ts
+++ src/lib/chain-implementations/LTC/MWEBPeg/index.ts
@@ -27,20 +27,27 @@ class MWEBPeg extends ChainImplementation {
 
         let pegout = false;
         let isPeg = false;
+        let peginValue = 0;
+        let pegoutValue = 0;
         for (let i = 0; i < transaction.vkern.length; i++) {
             const vkern = transaction.vkern[i];
             if(Number(vkern.pegin) > 0) {
                 pegout = false;
                 isPeg = true;
+                peginValue += Number(vkern.pegin);
             }
             if(vkern.pegout.length){
                 pegout = true;
                 isPeg = true;
+                for (let j = 0; j < vkern.pegout.length; j++) {
+                    const pegoutObj = vkern.pegout[j];
+                    pegoutValue += pegoutObj.value || 0;
+                }
             }
         }
         if(isPeg){
-            transaction.extras.houseContent = pegout ? "MWEB Pegout" : "MWEB Pegin";
-            transaction.extras[pegout?'mwebpegout':'mwebpegin'] = true;
+            transaction.extras.houseContent = pegout ? "MWEB Pegout - " + (pegoutValue || 0) + " LTC" : "MWEB Pegin - " + (peginValue || 0) + " LTC";
+            transaction.extras[pegout?'mwebpegout':'mwebpegin'] = pegout?pegoutValue:peginValue;
         }
         // transaction.mweb = true;
         // transaction.extras.mweb = true;
