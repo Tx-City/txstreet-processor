edec7fe91ed9d9163ffb7f3f3fbbc85869374855
web3dopamine <dhaileytaha@gmail.com>
2024-08-17 14:17:34 +0800

    house update for cash tokens

--- src/lib/chain-implementations/BCH/CashTokens/index.ts
+++ src/lib/chain-implementations/BCH/CashTokens/index.ts
@@ -1,24 +1,27 @@
-import ChainImplementation from '../../implementation'; 
+import ChainImplementation from '../../implementation';
 import mongodb from "../../../../databases/mongodb";
+import fetch from 'node-fetch';
 
 class CashTokens extends ChainImplementation {
-    public addresses: string[] = []; 
-    public _what: any = {}; 
+    public addresses: string[] = [];
+    public _what: any = {};
+    private rpcUrl: string = 'http://65.109.115.131:8332';
+    private rpcUser: string = 'user';
+    private rpcPass: string = 'pass';
 
     async init(): Promise<ChainImplementation> {
         try {
-            // Obtain addresses 
-            if(process.env.USE_DATABASE !== "true")
-                return this; 
+            if (process.env.USE_DATABASE !== "true")
+                return this;
             const { database } = await mongodb();
-            const collection = database.collection('houses'); 
-            const house = await collection.findOne({ name: 'cashtokens', chain: 'BCH' }); 
-            // console.log(`Initialized cashtokens`, house);
-            // addToCommonAddresses(addresses)
+            const collection = database.collection('houses');
+            const house = await collection.findOne({ name: 'cashtokens', chain: 'BCH' });
+            console.log(`Initialized CASHTOKEN HOUSE`, house);
+            
         } catch (error) {
             console.error(error);
         } finally {
-            return this; 
+            return this;
         }
     }
 
@@ -31,19 +34,71 @@ class CashTokens extends ChainImplementation {
         let found = false;
 
         console.log("cashtokens transaction hash======", transaction.hash);
-        
-            // if (transaction?.tokenData !== undefined) {
-            //   console.log('tokenData exists:', transaction.tokenData);
-            //   transaction.house = 'cashtokens';
-            //   return true;
-            // }
+        const hash = transaction.hash;
 
-            // console.log('tokenData does not exist');
-            
-        return true;  
+        try {
+            const transactionData = await this.getRawTransaction(hash);
+            console.log("Raw transaction data:", transactionData);
+            // Process the transaction data as needed
+            if (transactionData && transactionData.vout) {
+                for (const output of transactionData.vout) {
+                    if (output.tokenData) {
+                        console.log("has inside cashtoken");
+                        if(!transaction.extras) 
+                            transaction.extras = {};
+                        // transaction.extras.houseContent = `there is a cashtoken`;
+                        // console.log(transaction.extras.houseContent + ' is the house content');
+                        transaction.house = 'cashtokens';
+                
+                        return true;
+                        break;  // We found a cashtoken, no need to continue checking
+                    } else  {
+                        console.log("NO cashtoken");
+                        return false;
+                    }
+                }
+            }
+           
 
+        } catch (error) {
+            console.error("Error fetching raw transaction:", error);
+            return false;
+        }
+       
     }
 
+    private async getRawTransaction(txid: string): Promise<any> {
+        const headers = {
+            'Authorization': 'Basic ' + Buffer.from(this.rpcUser + ":" + this.rpcPass).toString('base64'),
+            'Content-Type': 'application/json'
+        };
+
+        const body = JSON.stringify({
+            jsonrpc: "1.0",
+            id: "curltest",
+            method: "getrawtransaction",
+            params: [txid, 2]
+        });
+
+        try {
+            const response = await fetch(this.rpcUrl, {
+                method: 'POST',
+                headers: headers,
+                body: body
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+    
+            return data.result;
+        } catch (error) {
+            console.error('There was an error fetching the raw transaction!', error);
+            throw error;
+        }
+    }
 }
 
-export default new CashTokens('BCH'); 
+export default new CashTokens('BCH');
